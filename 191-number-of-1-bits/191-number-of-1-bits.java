6c72aefedd03c39b393f08d26b9f5a9c4e277d86
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-05-27 00:29:21 +0530

    Time: 2 ms (18.10%), Space: 41.3 MB (39.75%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.java
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -3,8 +3,8 @@ public class Solution {
     public int hammingWeight(int n) {
         String s=Integer.toBinaryString(n);
         int res=0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='1'){
+        for(char x:s.toCharArray()){
+            if(x=='1'){
                 res++;
             }
         }
