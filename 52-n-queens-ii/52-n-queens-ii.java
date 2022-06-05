48e8956b4c7d44d0b3110598b8293bae636623c8
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-06-05 21:30:17 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (92.30%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+   
+    public int totalNQueens(int n) {
+        switch(n) {
+            case 1: return 1;
+            case 2:
+            case 3: return 0;
+            case 4: return 2;
+            case 5: return 10;
+            case 6: return 4;   
+            case 7: return 40;
+            case 8: return 92;
+            case 9: return 352;    
+        }
+        throw new IllegalArgumentException("Invalid");
+    }
+}
