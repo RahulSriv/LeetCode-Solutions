a8ba06291446befd366802880ae91f368bafd467
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-06-07 23:36:50 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (58.75%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -9,8 +9,8 @@ class Solution {
                 nums1[k--]=nums2[j--];
             }
         }
-        while(i>=0)
-            nums1[k--]=nums1[i--];
+        // while(i>=0)
+        //     nums1[k--]=nums1[i--];
         while(j>=0)
             nums1[k--]=nums2[j--];
     }
