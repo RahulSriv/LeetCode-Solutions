22e3d46780eb47fffc7cb3f38acce729c54cf684
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-06-09 22:00:48 +0530

    Time: 3 ms (31.42%), Space: 50.7 MB (9.82%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,17 +1,31 @@
+// Using Binary Search
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int[] res=new int[2];
-        HashMap<Integer, Integer> m=new HashMap<Integer, Integer>();
         for(int i=0;i<numbers.length;i++){
-            m.put(numbers[i],i);
-        }
-        for(int i=0;i<numbers.length;i++){
-            if(m.containsKey(target-numbers[i]) && i!=m.get(target-numbers[i])){
-                res[0]=i+1;
-                res[1]=m.get(target-numbers[i])+1;
-                break;
+            int j=Arrays.binarySearch(numbers,i+1,numbers.length,target-numbers[i]);
+            if(j>0){
+                return new int[] {i+1,j+1};
             }
         }
-        return res;
+        return null;
     }
 }
+
+// // Using HashMap
+// class Solution {
+//     public int[] twoSum(int[] numbers, int target) {
+//         int[] res=new int[2];
+//         HashMap<Integer, Integer> m=new HashMap<Integer, Integer>();
+//         for(int i=0;i<numbers.length;i++){
+//             m.put(numbers[i],i);
+//         }
+//         for(int i=0;i<numbers.length;i++){
+//             if(m.containsKey(target-numbers[i]) && i!=m.get(target-numbers[i])){
+//                 res[0]=i+1;
+//                 res[1]=m.get(target-numbers[i])+1;
+//                 break;
+//             }
+//         }
+//         return res;
+//     }
+// }
