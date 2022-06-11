fc4a88f6efa0c851e6f5cfad1ebe6c4553010e07
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-06-12 02:42:48 +0530

    Time: 4 ms (87.69%), Space: 59.8 MB (77.69%) - LeetHub

--- 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
+++ 1685-sum-of-absolute-differences-in-a-sorted-array/1685-sum-of-absolute-differences-in-a-sorted-array.java
@@ -2,16 +2,11 @@ class Solution {
     public int[] getSumAbsoluteDifferences(int[] nums) {
         int i,n=nums.length;
         int[] res=new int[n];
-        int[] presum=new int[n];
-        presum[0]=nums[0];
+        for(i=1;i<n;i++)
+            nums[i]+=nums[i-1];
+        res[0]=-((n-1)*nums[0])+(nums[n-1]-nums[0]); 
         for(i=1;i<n;i++){
-            presum[i]=nums[i];
-            presum[i]+=presum[i-1];
-        }
-        int sum=nums[n-1];
-        res[0]=-((n-1)*nums[0])+(presum[n-1]-presum[0]); 
-        for(i=1;i<n;i++){
-            res[i]=i*nums[i]-presum[i-1]-((n-1-i)*nums[i])+(presum[n-1]-presum[i]);
+            res[i]=(i*(nums[i]-nums[i-1]))-nums[i-1]-((n-1-i)*(nums[i]-nums[i-1]))+(nums[n-1]-nums[i]);
         }
         return res;
     }
