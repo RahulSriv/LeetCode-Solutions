19407c0fc65ed7644194585072ae4637db117e73
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-06-19 19:43:10 +0530

    Time: 244 ms (19.79%), Space: 72.5 MB (42.88%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        List<List<String>> res=new ArrayList<>();
+        String s="";
+        for(int i=0;i<searchWord.length();i++){
+            char c=searchWord.charAt(i);
+            s=s+String.valueOf(c);
+            System.out.println(s);
+            ArrayList<String> sl=new ArrayList<String>();
+            Arrays.sort(products);
+            for(int j=0;j<products.length;j++){
+                String t=products[j];
+                if(sl.size()<3 && t.startsWith(s))
+                    sl.add(t);
+            }
+            System.out.println(sl);
+            res.add(new ArrayList<String>(sl));
+            sl.clear();
+        }
+        return res;
+    }
+}
