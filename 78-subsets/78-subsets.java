0824850c095614508d3e2df3c368d65cc424954c
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-06-24 23:59:11 +0530

    Time: 2 ms (35.16%), Space: 43.1 MB (64.69%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -2,7 +2,7 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res=new ArrayList<>();
         for(int i=0;i<1<<nums.length;i++){
-            List<Integer> al=new ArrayList<Integer>();
+            List<Integer> al=new ArrayList<>();
             for(int j=0;j<nums.length;j++){
                 if((i&1<<j)!=0)
                     al.add(nums[j]);
