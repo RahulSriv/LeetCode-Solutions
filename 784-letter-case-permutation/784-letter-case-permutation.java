df7278bd55a8a952ac480143651960f4273addcd
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-06-29 17:28:35 +0530

    Time: 14 ms (27.44%), Space: 54.7 MB (19.39%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.java
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -1,23 +1,23 @@
 class Solution {
     public List<String> res=new ArrayList<String>();
-    public List<String> getCombinations(String r, String s, int ind){
-        if(ind==s.length()){
+    public List<String> getCombinations(String r, String s){
+        if(s.length()==0){
             res.add(r);
             return null;
         }
-        if(s.charAt(ind)>='0' && s.charAt(ind)<='9'){
-            char z=s.charAt(ind);
-            getCombinations(r+z,s,ind+1);
+        if(s.charAt(0)>='0' && s.charAt(0)<='9'){
+            char z=s.charAt(0);
+            getCombinations(r+z,s.substring(1));
         }
         else {
-            char x=s.charAt(ind);
+            char x=s.charAt(0);
             char y=Character.isLowerCase(x)?Character.toUpperCase(x):Character.toLowerCase(x);
-            getCombinations(r+x,s,ind+1);
-            getCombinations(r+y,s,ind+1);
+            getCombinations(r+x,s.substring(1));
+            getCombinations(r+y,s.substring(1));
         }
         return res;
     }
     public List<String> letterCasePermutation(String s) {
-        return getCombinations("",s,0);
+        return getCombinations("",s);
     }
 }
