713fcbd87c52d4bf73937beac662481c4f267810
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-06-29 16:42:12 +0530

    Time: 1 ms (88.24%), Space: 42.8 MB (77.16%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,15 +1,25 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
-        for(int i=0;i<1<<nums.length;i++){
-            List<Integer> al=new ArrayList<>();
-            for(int j=0;j<nums.length;j++){
-                if((i&1<<j)!=0)
-                    al.add(nums[j]);
-            }
-            res.add(new ArrayList<Integer>(al));
-            al.clear();
+    
+    public List<List<Integer>> res=new ArrayList<>();
+    public List<List<Integer>> allSubsets(List<Integer> op, List<Integer> ip){
+        if(ip.isEmpty()){
+            res.add(new ArrayList<Integer>(op));
+            return res;
         }
+        op.add(ip.get(0));
+        allSubsets(op,ip.subList(1,ip.size()));
+        op.remove(op.size()-1);
+        allSubsets(op,ip.subList(1,ip.size()));
         return res;
+    }
+    
+    public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> ip=new ArrayList<Integer>();
+        List<Integer> op=new ArrayList<Integer>();
+        for(int x:nums)
+            ip.add(x);
+        
+        return allSubsets(op,ip);
+        
     }
 }
