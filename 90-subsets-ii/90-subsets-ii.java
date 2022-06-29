ba8a5c78aa06583e355be5805a7fca28cde57e4b
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-06-29 16:55:06 +0530

    Time: 22 ms (5.34%), Space: 44.7 MB (12.80%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.java
+++ 90-subsets-ii/90-subsets-ii.java
@@ -1,41 +1,43 @@
-// class Solution {
-//     public List<List<Integer>> res=new ArrayList<>();
-//     public List<List<Integer>> allSubsets(List<Integer> op,List<Integer> ip){
-//         if(ip.size()==0){
-//             if(!res.contains(new ArrayList<Integer>(op)))
-//                 res.add(new ArrayList<Integer>(op));
-//             return res;
-//         }
-//         op.add(ip.get(0));
-//         allSubsets(op,ip.subList(1,ip.size()));
-//         op.remove(op.size()-1);
-//         allSubsets(op,ip.subList(1,ip.size()));
-//         return res;
-//     }
-//     public List<List<Integer>> subsetsWithDup(int[] nums) {
-//         List<Integer> ip=new ArrayList<Integer>();
-//         List<Integer> op=new ArrayList<Integer>();
-//         Arrays.sort(nums);
-//         for(int num:nums) 
-//             ip.add(num);
-//         return allSubsets(op,ip);
-//     }
-// }
-
+// recursive method (take or reject)
 class Solution {
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> res=new ArrayList<>();
-        Arrays.sort(nums);
-        for(int i=0;i<1<<nums.length;i++){
-            List<Integer> al=new ArrayList<Integer>();
-            for(int j=0;j<nums.length;j++){
-                if((i&1<<j)!=0)
-                    al.add(nums[j]);
-            }
-            if(res.contains(new ArrayList<Integer>(al))==false)
-                res.add(new ArrayList<Integer>(al));
-            al.clear();
+    public List<List<Integer>> res=new ArrayList<>();
+    public List<List<Integer>> allSubsets(List<Integer> op,List<Integer> ip){
+        if(ip.size()==0){
+            if(!res.contains(new ArrayList<Integer>(op)))
+                res.add(new ArrayList<Integer>(op));
+            return res;
         }
+        op.add(ip.get(0));
+        allSubsets(op,ip.subList(1,ip.size()));
+        op.remove(op.size()-1);
+        allSubsets(op,ip.subList(1,ip.size()));
         return res;
     }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<Integer> ip=new ArrayList<Integer>();
+        List<Integer> op=new ArrayList<Integer>();
+        Arrays.sort(nums);
+        for(int num:nums) 
+            ip.add(num);
+        return allSubsets(op,ip);
+    }
 }
+
+// // Iterative using power set method
+// class Solution {
+//     public List<List<Integer>> subsetsWithDup(int[] nums) {
+//         List<List<Integer>> res=new ArrayList<>();
+//         Arrays.sort(nums);
+//         for(int i=0;i<1<<nums.length;i++){
+//             List<Integer> al=new ArrayList<Integer>();
+//             for(int j=0;j<nums.length;j++){
+//                 if((i&1<<j)!=0)
+//                     al.add(nums[j]);
+//             }
+//             if(res.contains(new ArrayList<Integer>(al))==false)
+//                 res.add(new ArrayList<Integer>(al));
+//             al.clear();
+//         }
+//         return res;
+//     }
+// }
