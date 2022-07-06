52a83d7796136ccc2ae8241f658f477fd6cd5b68
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-07-06 15:10:53 +0530

    Time: 7 ms (57.14%), Space: 42.8 MB (73.81%) - LeetHub

--- 903-valid-permutations-for-di-sequence/903-valid-permutations-for-di-sequence.java
+++ 903-valid-permutations-for-di-sequence/903-valid-permutations-for-di-sequence.java
@@ -1,24 +1,25 @@
 class Solution {
-    public int numPermsDISequence(String S) {
-        int n = S.length(), M = (int)1e9 + 7;
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, 1);
-        for (int i = 1; i <= n; i++) {
-            int[] temp = new int[n + 1];
-            for (int j = 0; j <= i; j++) {
-                temp[j] = j == 0 ? 0 : temp[j - 1];
-                if (S.charAt(i - 1) == 'D') {
-                    temp[j] += (dp[i - 1] - (j == 0 ? 0 : dp[j - 1])) % M;
-                    if (temp[j] < 0) {
-                        temp[j] += M;
+    public int numPermsDISequence(String str) {
+        int n=str.length(), mod=(int)1e9+7;
+        int[] dputil=new int[n+1];
+        Arrays.fill(dputil, 1);
+        for(int i=1;i<=n;i++){
+            int[] save=new int[n+1];
+            for(int j=0;j<=i;j++) {
+                save[j]=(j==0?0:save[j-1]);
+                if (str.charAt(i-1)=='D') {
+                    save[j]+=(dputil[i-1]-(j==0?0:dputil[j-1]))%mod;
+                    if (save[j]<0) {
+                        save[j]+=mod;
                     }
-                } else {
-                    temp[j] += j == 0 ? 0 : dp[j - 1];
+                } 
+                else {
+                    save[j]+=(j==0?0:dputil[j-1]);
                 }
-                temp[j] %= M;
+                save[j]%=mod;
             }
-            dp = temp;
+            dputil=save;
         }
-        return dp[n];
+        return dputil[n];
     }
 }
