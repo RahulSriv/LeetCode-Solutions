ac995acb70de06b72cd07d07042d20540eabdf17
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-07-12 11:30:13 +0530

    Time: 4 ms (93.91%), Space: 42 MB (49.74%) - LeetHub

---
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean makesquare(int[] M) {
+        Arrays.sort(M);
+        int total = 0;
+        for (int i = 0; i < M.length; i++)
+            total += M[i];
+        side = total / 4;
+        if ((float)total / 4 > side || M[M.length-1] > side)
+            return false;
+        return btrack(M.length-1, side, 0, M);
+    }
+    private int side;
+    private boolean btrack(int i, int space, int done, int[] M) {
+        if (done == 3)
+            return true;
+        for (; i >= 0; i--) {
+            int num = M[i];
+            boolean res;
+            if (num > space)
+                continue;
+            M[i] = side + 1;
+            if (num == space)
+                res = btrack(M.length-2, side, done+1, M);
+            else
+                res = btrack(i-1, space-num, done, M);
+            if (res)
+                return true;
+            M[i] = num;
+            while (i > 0 && M[i-1] == num)
+                i--;
+        }
+        return false;
+    }
+}
