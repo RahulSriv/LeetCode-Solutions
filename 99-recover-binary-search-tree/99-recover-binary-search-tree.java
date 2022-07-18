06ee0673f102f71e404de7874f763e5ae9c529bd
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-07-18 20:12:01 +0530

    Time: 2 ms (100.00%), Space: 42.4 MB (92.82%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -16,16 +16,15 @@
 class Solution {
     TreeNode first=null, second=null, prev=null;
     public void inorder(TreeNode root){
-        if(root==null)
-            return ;
-        inorder(root.left);
-        if(first==null && (prev==null || prev.val>=root.val))
-            first=prev;
-        if(first!=null && (prev==null || prev.val>=root.val))
-            second=root;
-        prev=root;
-        inorder(root.right);
-        
+        if(root!=null){
+            inorder(root.left);
+            if(first==null && (prev==null || prev.val>=root.val))
+                first=prev;
+            if(first!=null && (prev==null || prev.val>=root.val))
+                second=root;
+            prev=root;
+            inorder(root.right);
+        }
     }
     public void recoverTree(TreeNode root) {
         if(root==null)
