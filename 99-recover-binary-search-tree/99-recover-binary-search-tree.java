0675d128ae018f25cd3b28186a444ee3d0bc5dc8
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-07-18 20:04:50 +0530

    Time: 3 ms (80.47%), Space: 46.6 MB (80.26%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// class Solution {
+//     TreeNode first=null;
+//     TreeNode second=null; 
+//     TreeNode prev=new TreeNode(Integer.MIN_VALUE);
+//     public void inorder(TreeNode root){
+//         if(root==null)
+//             return ;
+//         inorder(root.left);
+//         if(first==null && prev.val>=root.val)
+//             first=prev;
+//         if(first!=null && prev.val>=root.val)
+//             second=root;
+//         prev=root;
+//         inorder(root.right);
+        
+//     }
+//     public void recoverTree(TreeNode root) {
+//         inorder(root);
+//         int t=first.val;
+//         first.val=second.val;
+//         second.val=t;
+//     }
+// }
+class Solution {
+    private TreeNode first;
+    private TreeNode second;
+    private TreeNode pre;
+    public void recoverTree(TreeNode root) {
+        if(root==null) return;
+        first = null;
+        second = null;
+        pre = null;
+        inorder(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+    
+    private void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.left);
+        
+        if(first==null && (pre==null ||pre.val>=root.val)){
+            first = pre;
+        }
+        if(first!=null && pre.val>=root.val){
+            second = root;
+        }
+        pre = root;
+        inorder(root.right);
+    }
+}
