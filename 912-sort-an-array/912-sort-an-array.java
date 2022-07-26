ebe7c12fa7fcfe00779bbe9bb50dfce3a3e74981
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-07-26 22:35:11 +0530

    Time: 24 ms (35.17%), Space: 70.7 MB (29.50%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,15 +1,21 @@
 class Solution {
     public void merge(int[] nums, int l, int m, int r){
-        int i, j, n1=m-l+1, n2=r-m;
+        int n1=m-l+1;
+        int n2=r-m;
         int[] left=new int[n1];
         int[] right=new int[n2];
-        for(i=0;i<n1;i++)
-            left[i]=nums[i+l];
-        for(j=0;j<n2;j++)
+        int i=0,j=0;
+        while(i<n1){
+            left[i]=nums[l+i];
+            i++;
+        }
+        while(j<n2){
             right[j]=nums[m+1+j];
-        int k=l;
-        i=0; 
+            j++;
+        }
+        i=0;
         j=0;
+        int k=l;
         while(i<n1 && j<n2){
             if(left[i]<right[j])
                 nums[k++]=left[i++];
@@ -21,16 +27,16 @@ class Solution {
         while(j<n2)
             nums[k++]=right[j++];
     }
-    public void mergeSort(int[] nums, int l, int r){
+    public void sort(int[] nums, int l, int r){
         if(l<r){
             int m=l+(r-l)/2;
-            mergeSort(nums,l,m);
-            mergeSort(nums,m+1,r);
+            sort(nums,l,m);
+            sort(nums,m+1,r);
             merge(nums,l,m,r);
         }
     }
     public int[] sortArray(int[] nums) {
-        mergeSort(nums,0,nums.length-1);
+        sort(nums,0,nums.length-1); 
         return nums;
     }
 }
