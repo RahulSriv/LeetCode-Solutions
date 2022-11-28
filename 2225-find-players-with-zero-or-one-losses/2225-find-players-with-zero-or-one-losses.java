0d3f244d18c94089b301a897f01fffd3c214eac8
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2022-11-28 20:35:42 +0530

    Time: 108 ms (86.17%), Space: 157.5 MB (34.42%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.java
@@ -1,26 +1,24 @@
 class Solution {
     public List<List<Integer>> findWinners(int[][] matches) {
-        List<Integer> ans0=new ArrayList<>();
-        List<Integer> ans1=new ArrayList<>();
-        Map<Integer, Integer> m=new HashMap<>();
-        for(int i=0;i<matches.length;i++)
-            m.put(matches[i][1],m.getOrDefault(matches[i][1],0)+1);
-        Set<Integer> seen=new HashSet<Integer>();
-        for(int i=0;i<matches.length;i++){
-            if(!seen.contains(matches[i][0]) && !m.containsKey(matches[i][0])){
-                seen.add(matches[i][0]);
-                ans0.add(matches[i][0]);
-            }
-            if(!seen.contains(matches[i][1]) && m.get(matches[i][1])==1){
-                ans1.add(matches[i][1]);
-                seen.add(matches[i][1]);
-            }
+        int[] count=new int[100001];
+        Arrays.fill(count,-1);
+        for(int[] m:matches){
+            if(count[m[0]]==-1)
+                count[m[0]]++;
+            if(count[m[1]]==-1)
+                count[m[1]]=1;
+            else
+                count[m[1]]++;
         }
-        Collections.sort(ans0);
-        Collections.sort(ans1);
-        List<List<Integer>> ans=new ArrayList<>();
-        ans.add(ans0);
-        ans.add(ans1);
+        List<List<Integer>> ans=Arrays.asList(new ArrayList<Integer>(), new ArrayList<Integer>());
+        for(int i=0;i<count.length;i++){
+            if(count[i]==0)
+                ans.get(0).add(i);
+            else if(count[i]==1)
+                ans.get(1).add(i);
+        }
+        Collections.sort(ans.get(0));
+        Collections.sort(ans.get(1));
         return ans;
     }
 }
