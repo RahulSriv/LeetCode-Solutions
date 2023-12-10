2320eaf04a6528b73fbe201a4b0987efeb4c2b86
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2023-12-10 20:39:15 +0530

    Time: 7 ms (71.97%), Space: 42 MB (48.54%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -4,22 +4,21 @@ class Solution {
             return false;
         int[] count1=new int[26];
         int[] count2=new int[26];
-        Arrays.fill(count1,0);
-        Arrays.fill(count2,0);
         for(int i=0;i<s1.length();i++){
             count1[s1.charAt(i)-'a']++;
             count2[s2.charAt(i)-'a']++;
         }
-        if(Arrays.equals(count1,count2))
+        if(Arrays.equals(count1, count2))
             return true;
         int i=0, j=s1.length();
-        for(;j<s2.length();j++){
+        while(j<s2.length()){
             count2[s2.charAt(i)-'a']--;
             count2[s2.charAt(j)-'a']++;
             if(Arrays.equals(count1,count2))
                 return true;
+            j++;
             i++;
-        }   
+        }        
         return false;
     }
 }
