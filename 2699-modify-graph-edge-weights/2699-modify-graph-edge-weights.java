7c57b95c12be9a012f31d0c29de9f52693fcccc8
Rahul Srivastava <75925218+RahulSriv@users.noreply.github.com>
2024-08-31 14:42:57 +0530

    Time: 252 ms (44.44%), Space: 51.2 MB (61.62%) - LeetHub

---
+++ 2699-modify-graph-edge-weights/2699-modify-graph-edge-weights.java
@@ -0,0 +1,77 @@
+class Solution {
+
+    private static final int INF = (int) 2e9;
+
+    public int[][] modifiedGraphEdges(
+        int n,
+        int[][] edges,
+        int source,
+        int destination,
+        int target
+    ) {
+        long currentShortestDistance = runDijkstra(
+            edges,
+            n,
+            source,
+            destination
+        );
+        if (currentShortestDistance < target) 
+            return new int[0][0];
+        boolean matchesTarget = (currentShortestDistance == target);
+        for (int[] edge : edges) {
+            if (edge[2] > 0) continue;
+            edge[2] = matchesTarget ? INF : 1;
+            if (!matchesTarget) {
+                long newDistance = runDijkstra(edges, n, source, destination);
+                if (newDistance <= target) {
+                    matchesTarget = true;
+                    edge[2] += target - newDistance;
+                }
+            }
+        }
+        return matchesTarget ? edges : new int[0][0];
+    }
+
+    private long runDijkstra(
+        int[][] edges,
+        int n,
+        int source,
+        int destination) {
+        long[][] adjMatrix = new long[n][n];
+        long[] minDistance = new long[n];
+        boolean[] visited = new boolean[n];
+
+        Arrays.fill(minDistance, INF);
+        for (long[] row : adjMatrix) {
+            Arrays.fill(row, INF);
+        }
+        minDistance[source] = 0;
+        for (int[] edge : edges) {
+            if (edge[2] != -1) {
+                adjMatrix[edge[0]][edge[1]] = edge[2];
+                adjMatrix[edge[1]][edge[0]] = edge[2];
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int nearestUnvisitedNode = -1;
+            for (int j = 0; j < n; ++j) {
+                if (
+                    !visited[j] &&
+                    (nearestUnvisitedNode == -1 ||
+                        minDistance[j] < minDistance[nearestUnvisitedNode])
+                ) {
+                    nearestUnvisitedNode = j;
+                }
+            }
+            visited[nearestUnvisitedNode] = true;
+            for (int v = 0; v < n; ++v) {
+                minDistance[v] = Math.min(
+                    minDistance[v],
+                    minDistance[nearestUnvisitedNode] +
+                    adjMatrix[nearestUnvisitedNode][v]
+                );
+            }
+        }
+        return minDistance[destination];
+    }
+}
